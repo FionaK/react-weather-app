1775e4ff30cbe34e1236dfed2a3fd58527fb735b
fk-owuor <katefiona121@gmail.com>
2019-03-19 17:28:30 +0300

    Add files

---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Counters extends Component {
+    state = {  }
+    render() { 
+        return (
+             <div>
+                 <counter />
+                 <counter />
+                 <counter />
+                 <counter />
+             </div> );
+    }
+}
+ 
+export default Counters;
